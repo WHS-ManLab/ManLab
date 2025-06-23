9caf063592eeb79c166c95026371f39f85e0594d
minyoung13 <mypark0113@naver.com>
2025-06-24 03:13:54 +0900

    feat: 로그 분석 결과 저장할 DB 추가

--- src/DBManager.cpp
+++ src/DBManager.cpp
@@ -1,43 +1,62 @@
 #include "DBManager.h"
 #include <iostream> //디버그용
 
-DBManager& DBManager::GetInstance() {
-    static DBManager instance;
-    return instance;
+DBManager &DBManager::GetInstance()
+{
+    static DBManager sInstance;
+    return sInstance;
 }
 
 DBManager::DBManager()
     : mHashStorage(sqlite_orm::make_storage(
-        "/ManLab/db/hash.db",
-        sqlite_orm::make_table("MalwareHashDB",
-            sqlite_orm::make_column("hashAlgo", &MalwareHashDB::hashAlgo),
-            sqlite_orm::make_column("malwareHash", &MalwareHashDB::malwareHash),
-            sqlite_orm::make_column("malwareName", &MalwareHashDB::malwareName)
-        )
-    )),
+          "/ManLab/db/hash.db",
+          sqlite_orm::make_table("MalwareHashDB",
+                                 sqlite_orm::make_column("hashAlgo", &MalwareHashDB::hashAlgo),
+                                 sqlite_orm::make_column("malwareHash", &MalwareHashDB::malwareHash),
+                                 sqlite_orm::make_column("malwareName", &MalwareHashDB::malwareName)))),
 
       mQuarantineStorage(sqlite_orm::make_storage(
-        "/ManLab/db/quarantine.db",
-        sqlite_orm::make_table("QuarantineMetadata",
-            sqlite_orm::make_column("OriginalPath", &QuarantineMetadata::OriginalPath),
-            sqlite_orm::make_column("QuarantinedFileName", &QuarantineMetadata::QuarantinedFileName),
-            sqlite_orm::make_column("OriginalSize", &QuarantineMetadata::OriginalSize),
-            sqlite_orm::make_column("QuarantineDate", &QuarantineMetadata::QuarantineDate),
-            sqlite_orm::make_column("QuarantineReason", &QuarantineMetadata::QuarantineReason),
-            sqlite_orm::make_column("MalwareNameOrRule", &QuarantineMetadata::MalwareNameOrRule)
-        )
-    ))
-{}
+          "/ManLab/db/quarantine.db",
+          sqlite_orm::make_table("QuarantineMetadata",
+                                 sqlite_orm::make_column("OriginalPath", &QuarantineMetadata::OriginalPath),
+                                 sqlite_orm::make_column("QuarantinedFileName", &QuarantineMetadata::QuarantinedFileName),
+                                 sqlite_orm::make_column("OriginalSize", &QuarantineMetadata::OriginalSize),
+                                 sqlite_orm::make_column("QuarantineDate", &QuarantineMetadata::QuarantineDate),
+                                 sqlite_orm::make_column("QuarantineReason", &QuarantineMetadata::QuarantineReason),
+                                 sqlite_orm::make_column("MalwareNameOrRule", &QuarantineMetadata::MalwareNameOrRule)))),
 
-void DBManager::InitSchema() {
-    mHashStorage.sync_schema();
-    mQuarantineStorage.sync_schema();
+      mLogAnalysisResultStorage(sqlite_orm::make_storage(
+          "/ManLab/db/logAnalysisResult.db",
+          sqlite_orm::make_table("LogAnalysisResult",
+                                 sqlite_orm::make_column("ID", &LogAnalysisResult::id, primary_key().autoincrement()),
+                                 sqlite_orm::make_column("Type", &LogAnalysisResult::type),
+                                 sqlite_orm::make_column("Description", &LogAnalysisResult::description),
+                                 sqlite_orm::make_column("Timestamp", &LogAnalysisResult::timestamp),
+                                 sqlite_orm::make_column("UID", &LogAnalysisResult::uid),
+                                 sqlite_orm::make_column("IsSuccess", &LogAnalysisResult::bIsSuccess),
+                                 sqlite_orm::make_column("OriginalLogPath", &LogAnalysisResult::originalLogPath),
+                                 sqlite_orm::make_column("RawLine", &LogAnalysisResult::rawLine))))
+{
 }
 
-StorageHash& DBManager::GetHashStorage() {
+void DBManager::InitSchema()
+{
+    mHashStorage.sync_schema();
+    mQuarantineStorage.sync_schema();
+    mLogAnalysisResultStorage.sync_schema();
+}
+
+StorageHash &DBManager::GetHashStorage()
+{
     return mHashStorage;
 }
 
-StorageQuarantine& DBManager::GetQuarantineStorage() {
+StorageQuarantine &DBManager::GetQuarantineStorage()
+{
     return mQuarantineStorage;
+}
+
+StorageLogAnalysisResult &DBManager::GetLogAnalysisResultStorage()
+{
+    return mLogAnalysisResultStorage;
 }
--- src/DBManager.h
+++ src/DBManager.h
@@ -1,30 +1,31 @@
 #pragma once
-#include <sqlite_orm.h> 
+#include <sqlite_orm.h>
 #include <string>
 
-/* 
+/*
 ============================================
 자신의 팀 전용 테이블과 DB를 다음 순서로 정의하세요.
 
 순서:
 1. 팀에서 사용할 데이터 구조 정의 (DBManager.h)
 2. 각각의 DB 파일에 대한 storage 타입 정의(DBManager.h)
-3. GETTER와 Storege 타입 변수 정의(DBManager.h)
+3. GETTER와 Storage 타입 변수 정의(DBManager.h)
 4. 생성자 초기화 리스트에 make_storage() 추가 (DBManager.cpp)
 5. storage.sync_schema()호출 추가 (DBManager.cpp)
 ============================================
 */
 
-
 // 악성코드 해시 DB 테이블 구조
-struct MalwareHashDB {
+struct MalwareHashDB
+{
     std::string hashAlgo;
     std::string malwareHash;
     std::string malwareName;
 };
 
 // 격리 메타데이터 DB 테이블 구조
-struct QuarantineMetadata {
+struct QuarantineMetadata
+{
     std::string OriginalPath;
     std::string QuarantinedFileName;
     long long OriginalSize;
@@ -33,38 +34,60 @@ struct QuarantineMetadata {
     std::string MalwareNameOrRule;
 };
 
-// 악성코드 해시에 대한 stroage타입 정의
+// 로그 분석 결과 DB 테이블 구조
+struct LogAnalysisResult
+{
+    int id;
+    std::string type;
+    std::string description;
+    std::string timestamp;
+    std::string uid;
+    bool bIsSuccess;
+    std::string originalLogPath;
+    std::string rawLine;
+};
+
+// 악성코드 해시에 대한 storage 타입 정의
 using StorageHash = decltype(sqlite_orm::make_storage("",
-    sqlite_orm::make_table("MalwareHashDB",
-        sqlite_orm::make_column("hashAlgo", &MalwareHashDB::hashAlgo),
-        sqlite_orm::make_column("malwareHash", &MalwareHashDB::malwareHash),
-        sqlite_orm::make_column("malwareName", &MalwareHashDB::malwareName)
-    )
-));
+                                                      sqlite_orm::make_table("MalwareHashDB",
+                                                                             sqlite_orm::make_column("hashAlgo", &MalwareHashDB::hashAlgo),
+                                                                             sqlite_orm::make_column("malwareHash", &MalwareHashDB::malwareHash),
+                                                                             sqlite_orm::make_column("malwareName", &MalwareHashDB::malwareName))));
 
-// 악성코드 격리 메타데이터에 대한 stroage타입 정의
+// 악성코드 격리 메타데이터에 대한 storage 타입 정의
 using StorageQuarantine = decltype(sqlite_orm::make_storage("",
-    sqlite_orm::make_table("QuarantineMetadata",
-        sqlite_orm::make_column("OriginalPath", &QuarantineMetadata::OriginalPath),
-        sqlite_orm::make_column("QuarantinedFileName", &QuarantineMetadata::QuarantinedFileName),
-        sqlite_orm::make_column("OriginalSize", &QuarantineMetadata::OriginalSize),
-        sqlite_orm::make_column("QuarantineDate", &QuarantineMetadata::QuarantineDate),
-        sqlite_orm::make_column("QuarantineReason", &QuarantineMetadata::QuarantineReason),
-        sqlite_orm::make_column("MalwareNameOrRule", &QuarantineMetadata::MalwareNameOrRule)
-    )
-));
+                                                            sqlite_orm::make_table("QuarantineMetadata",
+                                                                                   sqlite_orm::make_column("OriginalPath", &QuarantineMetadata::OriginalPath),
+                                                                                   sqlite_orm::make_column("QuarantinedFileName", &QuarantineMetadata::QuarantinedFileName),
+                                                                                   sqlite_orm::make_column("OriginalSize", &QuarantineMetadata::OriginalSize),
+                                                                                   sqlite_orm::make_column("QuarantineDate", &QuarantineMetadata::QuarantineDate),
+                                                                                   sqlite_orm::make_column("QuarantineReason", &QuarantineMetadata::QuarantineReason),
+                                                                                   sqlite_orm::make_column("MalwareNameOrRule", &QuarantineMetadata::MalwareNameOrRule))));
 
-class DBManager {
+// 로그 분석 결과에 대한 storage 타입 정의
+using StorageLogAnalysisResult = decltype(sqlite_orm::make_storage("",
+                                                                   sqlite_orm::make_table("LogAnalysisResult",
+                                                                                          sqlite_orm::make_column("ID", &LogAnalysisResult::id, primary_key().autoincrement()),
+                                                                                          sqlite_orm::make_column("Type", &LogAnalysisResult::type),
+                                                                                          sqlite_orm::make_column("Description", &LogAnalysisResult::description),
+                                                                                          sqlite_orm::make_column("Timestamp", &LogAnalysisResult::timestamp),
+                                                                                          sqlite_orm::make_column("UID", &LogAnalysisResult::uid),
+                                                                                          sqlite_orm::make_column("IsSuccess", &LogAnalysisResult::bIsSuccess),
+                                                                                          sqlite_orm::make_column("OriginalLogPath", &LogAnalysisResult::originalLogPath),
+                                                                                          sqlite_orm::make_column("RawLine", &LogAnalysisResult::rawLine))));
+
+class DBManager
+{
 public:
     static DBManager& GetInstance();
     void InitSchema();
     DBManager(const DBManager&) = delete;
-    DBManager& operator=(const DBManager&) = delete;
+    DBManager &operator=(const DBManager&) = delete;
 
-    //Getter
+    // Getter
     StorageHash& GetHashStorage();
     StorageQuarantine& GetQuarantineStorage();
-
+    StorageLogAnalysisResult& GetLogAnalysisResultStorage();
 
 private:
     DBManager();
@@ -72,4 +95,5 @@ private:
     // storage
     StorageHash mHashStorage;
     StorageQuarantine mQuarantineStorage;
+    StorageLogAnalysisResult mLogAnalysisResultStorage;
 };
