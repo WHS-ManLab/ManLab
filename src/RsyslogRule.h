35899520d4c1efe3112b49325e93339145f32fda
jaeman1 <50677041+jaeman1@users.noreply.github.com>
2025-07-14 15:36:48 +0900

    Revert "Feature : RsyslogRule 추가 및 코드 리팩토링 진행"

--- src/RsyslogRule.h
+++ src/RsyslogRule.h
@@ -6,5 +6,3 @@
 #include "RsyslogManager.h"  // LogEntry, AnalysisResult 사용
 
 AnalysisResult AnalyzeSudoLog(const LogEntry& entry, const std::unordered_set<std::string>& rsyslogRuleSet);
-AnalysisResult AnalyzePasswdChangeLog(const LogEntry& entry, const std::deque<LogEntry>& recentLogs, const std::unordered_set<std::string>& rsyslogRuleSet);
-AnalysisResult AnalyzePasswordFailureLog(const LogEntry& entry);
