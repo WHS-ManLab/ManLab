5d3fe95f90fd9f388690c649feb381f94a783b87
jaeman1 <simwin.jae.man@gmail.com>
2025-07-15 00:28:44 +0900

    feat: 개편된 구조에 맞게 수동검사 코드 수정

--- src/FIMBaselineGenerator.cpp
+++ src/FIMBaselineGenerator.cpp
@@ -36,10 +36,10 @@ std::string BaselineGenerator::compute_md5(const std::string& filepath) {
 }
 
 //INI 파일에서 경로를 파싱하고 해당 경로와 파일/디랙토리 해시를 DB에 저장
-void BaselineGenerator::parse_ini_and_store() {
+void BaselineGenerator::parse_ini_and_store(std::ostream& out) {
     std::ifstream ini_file(ini_path_);
     if (!ini_file) {
-        std::cerr << "[ERROR] INI 파일 열기 실패: " << ini_path_ << std::endl;
+        out << "[ERROR] INI 파일 열기 실패: " << ini_path_ << std::endl;
         return;
     }
 
@@ -54,13 +54,13 @@ void BaselineGenerator::parse_ini_and_store() {
                 path.erase(0, path.find_first_not_of(" \t"));
                 path.erase(path.find_last_not_of(" \t") + 1);
 
-                std::cout << "[INI] 경로 파싱됨: " << path << std::endl;
+                out << "[INI] 경로 파싱됨: " << path << std::endl;
 
                 try {
                     if (std::filesystem::is_regular_file(path)) {
                         //단일 파일인 경우 해시 계산 후 DB에 저장
                         std::string hash = compute_md5(path);
-                        std::cout << "[단일 파일] 저장 중: " << path << " → " << hash << std::endl;
+                        out << "[단일 파일] 저장 중: " << path << " → " << hash << std::endl;
                         storage.replace(BaselineEntry{path, hash});
                     }
                     else if (std::filesystem::is_directory(path)) {
@@ -69,21 +69,21 @@ void BaselineGenerator::parse_ini_and_store() {
                             if (entry.is_regular_file()) {
                                 std::string file_path = entry.path().string();
                                 std::string hash = compute_md5(file_path);
-                                std::cout << "[디렉토리 파일] 저장 중: " << file_path << " → " << hash << std::endl;
+                                out << "[디렉토리 파일] 저장 중: " << file_path << " → " << hash << std::endl;
                                 storage.replace(BaselineEntry{file_path, hash});
                             }
                         }
                     }
                     else {
-                        std::cerr << "[ERROR] 유효하지 않은 경로입니다: " << path << std::endl;
+                        out << "[ERROR] 유효하지 않은 경로입니다: " << path << std::endl;
                     }
                 } catch (const std::filesystem::filesystem_error& e) {
-                    std::cerr << "[ERROR] 파일/디렉토리 접근 실패: " << e.what() << std::endl;
+                    out << "[ERROR] 파일/디렉토리 접근 실패: " << e.what() << std::endl;
                 }
             }
         }
     }
 }
-void BaselineGenerator::generate_and_store() {
-    parse_ini_and_store();
+void BaselineGenerator::generate_and_store(std::ostream& out) {
+    parse_ini_and_store(out);
 }
