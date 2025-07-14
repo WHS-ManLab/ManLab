dd7811a5280704a2659a72fd0ca69fb1ab946752
jaeman1 <simwin.jae.man@gmail.com>
2025-07-15 00:28:44 +0900

    feat: 개편된 구조에 맞게 수동검사 코드 수정

--- src/FIMBaselineGenerator.h
+++ src/FIMBaselineGenerator.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <string>
+#include <ostream>
 
 class BaselineGenerator {
 public:
     BaselineGenerator(const std::string& ini_path, const std::string& db_path);
-    void generate_and_store();
+    void generate_and_store(std::ostream& out);
 
     static std::string compute_md5(const std::string& filepath);
 
@@ -12,5 +13,5 @@ private:
     std::string ini_path_;
     std::string db_path_;
 
-    void parse_ini_and_store();
+    void parse_ini_and_store(std::ostream& out);
 };
