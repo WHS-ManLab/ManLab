49880d20704a30bcd52874df253e832ab553eeb2
9yuBit <thsqjarb@gmail.com>
2025-07-15 14:07:57 +0900

    feat : 룰 추가 및 RsyslogManager 리팩토링

--- src/RsyslogRule.h
+++ src/RsyslogRule.h
@@ -6,3 +6,9 @@
 #include "RsyslogManager.h"  // LogEntry, AnalysisResult 사용
 
 AnalysisResult AnalyzeSudoLog(const LogEntry& entry, const std::unordered_set<std::string>& rsyslogRuleSet);
+AnalysisResult AnalyzePasswdChangeLog(const LogEntry& entry, const std::deque<LogEntry>& recentLogs, const std::unordered_set<std::string>& rsyslogRuleSet);
+AnalysisResult AnalyzePasswordFailureLog(const LogEntry& entry);
+AnalysisResult AnalyzeSudoGroupChangeLog(const LogEntry& entry);
+AnalysisResult AnalyzeUserChangeLog(const LogEntry& entry);
+AnalysisResult AnalyzeGroupChangeLog(const LogEntry& entry);
+AnalysisResult AnalyzeGroupMemberChangeLog(const LogEntry& entry);
