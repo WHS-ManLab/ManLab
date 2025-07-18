bf5eb0886278a868d548c13bc45fd309d3214b8a
jaeman1 <simwin.jae.man@gmail.com>
2025-07-19 02:37:41 +0900

    modify: 전반적인 로깅 수정

--- install.sh
+++ install.sh
@@ -63,5 +63,9 @@ systemctl enable "$SERVICE_UNIT"
 echo "[INFO] Copying malware_hashes.txt..."
 cp -v "$MALHASH_SRC" "$MALHASH_DST"
 
+# 8. spdlog 설치
+echo "[INFO] Installing spdlog..."
+sudo apt install libspdlog-dev
+
 echo "[OK] ManLab installation completed."
 echo "▶ 설치가 완료되었습니다. 제품을 실행하려면 다음 명령어를 입력하세요: sudo ManLab run"
