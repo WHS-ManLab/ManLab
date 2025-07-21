0c1c106e6f05bc373186fea0231601656d1c7f14
rlacofla <rlacofla@ubuntu20.04.myguest.virtualbox.org>
2025-07-21 21:36:04 +0900

    fix : realtime db, 로깅 수정

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -7,6 +7,8 @@
 #include "INIReader.h"
 #include "Paths.h"
 
+//std::shared_ptr<spdlog::logger> RealTime_logger = nullptr;
+
 void InitLogger() 
 {
     // 최대 5MB, 최대 3개 파일 보관 (manlab.log, manlab.log.1, manlab.log.2)
@@ -19,6 +21,17 @@ void InitLogger()
     spdlog::set_default_logger(logger);
     spdlog::set_level(spdlog::level::debug);
     spdlog::flush_on(spdlog::level::debug);
+
+    //FIM 실시간 검사 로깅 선언
+    auto RealTime_logger = spdlog::rotating_logger_mt(
+        "RealTime_logger", "/root/ManLab/log/RealTimeMonitor.log",
+        1024 * 1024 * 5, // 최대 파일 크기 5MB
+        3                // 최대 파일 개수 3개
+    );
+    
+    RealTime_logger->set_level(spdlog::level::info);
+    RealTime_logger->flush_on(spdlog::level::info);
+
 }
 
 int main(int argc, char* argv[])
