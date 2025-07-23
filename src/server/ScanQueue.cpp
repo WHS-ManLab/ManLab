29a04528d4d64a3fbb67f3fdbfdb7942b1510603
vboxuser <vboxuser@manlab.myguest.virtualbox.org>
2025-07-23 22:03:54 +0900

    feat(realtimescan) : 실시간 검사 파일 실행 이벤트로 수정

--- src/server/ScanQueue.cpp
+++ src/server/ScanQueue.cpp
@@ -1,24 +1,30 @@
 #include "ScanQueue.h"
 
-void ScanQueue::Push(const std::string& path) 
+void ScanQueue::Push(ScanRequest&& req)
 {
     std::lock_guard<std::mutex> lock(mMutex);
-    mQueue.push(path);
+    mQueue.push(std::move(req));
     mCond.notify_one();
 }
 
-bool ScanQueue::Pop(std::string& outPath) 
+bool ScanQueue::Pop(ScanRequest& outReq)
 {
     std::unique_lock<std::mutex> lock(mMutex);
-    mCond.wait(lock, [&]() { return !mQueue.empty() || mbStop; });
+    mCond.wait(lock, [&]() {
+        return !mQueue.empty() || mbStop;
+    });
 
-    if (mbStop && mQueue.empty()) return false;
-    outPath = mQueue.front();
+    if (mbStop && mQueue.empty()) 
+    {
+        return false;
+    }
+
+    outReq = std::move(mQueue.front());
     mQueue.pop();
     return true;
 }
 
-void ScanQueue::Stop() 
+void ScanQueue::Stop()
 {
     std::lock_guard<std::mutex> lock(mMutex);
     mbStop = true;
