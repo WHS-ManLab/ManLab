0eb0e25d8e3591968c154069ac54d11b40711fc8
20221103@sungshin.ac.kr <20221103@sungshin.ac.kr>
2025-07-25 01:07:08 +0900

    fix : 코딩 컨벤션 수정

--- src/server/FIMIntegScan.h
+++ src/server/FIMIntegScan.h
@@ -4,4 +4,5 @@
 
 // 기준선 DB와 현재 파일 상태를 비교하여 무결성 검사 수행
 // verbose=true: 콘솔 출력 활성화
-void compare_with_baseline(bool verbose, std::ostream& out);
+void CompareWithBaseline(bool bVerbose, std::ostream& out);
+
